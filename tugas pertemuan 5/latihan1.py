c9c8f4e17765238ce0a3d4c8482712002825fe18
plugnowplay <115965039+plugnowplay@users.noreply.github.com>
2022-10-21 21:56:00 +0700

    Add files via upload

---
+++ tugas pertemuan 5/latihan1.py
@@ -0,0 +1,4 @@
+#menampilkan tulisan 'Hello' di layar
+print("Hello")
+#menampilkan tulisan 'Saya sedang belajar python' di layar
+print("Saya sedang belajar python")
---
+++ tugas pertemuan 5/latihan2.py
@@ -0,0 +1,7 @@
+# menjumlahkan dua bilangan menggunakan variabel a & b
+a = 8
+b = 6
+
+print("Variabel a =",a)
+print("Variabel b =",b)
+print("hasil penjumlahan a + b =", a+b)
